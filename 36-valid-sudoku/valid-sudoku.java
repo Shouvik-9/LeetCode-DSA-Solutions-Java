32f04d26eae01e766edee3cbc33b24ad40dfd6ed
Shouvik Dutta <142233626+Shouvik-9@users.noreply.github.com>
2026-05-08 13:09:46 +0530

    Time: 14 ms (8.28%) | Memory: 52.6 MB (7.13%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+                char current = board[row][col];
+
+                if (current == '.') {
+                    continue;
+                }
+                String rowKey = current + " in row " + row;
+                String colKey = current + " in col " + col;
+                String boxKey = current + " in box " + (row / 3) + "-" + (col / 3);
+
+                if (set.contains(rowKey) ||
+                    set.contains(colKey) ||
+                    set.contains(boxKey)) {
+                    return false;
+                }
+                set.add(rowKey);
+                set.add(colKey);
+                set.add(boxKey);
+            }
+        }
+        return true;
+    }
+}
